08cd2000f3363e9c32ce45a29ef640acf3b57950
StephanMagnin <63318991+StephanMagnin@users.noreply.github.com>
2021-05-28 18:20:52 +0200

    Add files via upload

---
+++ media/sounds/data.array.js
@@ -0,0 +1,11 @@
+const data = {
+  'E': {sound: 'media/sounds/drum1.mp3'},
+  'R': {sound: 'media/sounds/drum2.mp3'},
+  'T': {sound: 'media/sounds/beat1.mp3'},
+  'D': {sound: 'media/sounds/beat2.mp3'},
+  'F': {sound: 'media/sounds/beat3.mp3'},
+  'G': {sound: 'media/sounds/lindrum.mp3'},
+  'C': {sound: 'media/sounds/percu1.mp3'},
+  'V': {sound: 'media/sounds/electro1.mp3'},
+  'B': {sound: 'media/sounds/electro2.mp3'},
+};
